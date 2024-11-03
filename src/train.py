c6dcb4c4320a098bce57e8a04ba8ce079367ba77
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2024-11-03 15:12:57 +0000

    updated training parameters

--- src/train.py
+++ src/train.py
@@ -3,10 +3,10 @@ from utils.config import TrainingConfig
 
 options = TrainingConfig(
     learning_rate=1e-4,
-    epochs=12,
-    batch_size=2,
-    scheduler_steps=[8, 11],
-    loss_accumulation_step=16,
+    epochs=5,
+    batch_size=3,
+    scheduler_steps=[3, 4],
+    loss_accumulation_step=10,
     scheduler_decay=0.1,
     model_save_interval=5,
     early_stoping_step=3,
