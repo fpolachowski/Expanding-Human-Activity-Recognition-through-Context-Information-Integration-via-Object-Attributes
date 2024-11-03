05ab3442caa13555d314503fd5c38c5426013dc6
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2024-11-03 15:08:05 +0000

    moved back to old version of dataset

--- src/dataset/EpicKitchen55.py
+++ src/dataset/EpicKitchen55.py
@@ -1,13 +1,12 @@
 import torch
 import os
 import random
+import json
 import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader, Subset
 from safetensors.torch import load_file
-from torch.nn.utils.rnn import pad_sequence
 from torchvision.transforms import functional as F
-import json
 
 from multiprocessing import Pool
 
@@ -21,9 +20,9 @@ except ImportError:
 class VideoRecord():
     def __init__(self, data, root_datapath):
         self._data = data
-        self._path = os.path.join(root_datapath,
-                                  data["participant_id"],
-                                  "rgb_frames",
+        self._path = os.path.join(root_datapath, 
+                                  data["participant_id"], 
+                                  "rgb_frames", 
                                   data["video_id"]
                                   )
 
@@ -50,41 +49,40 @@ class VideoRecord():
     @property
     def activity(self):
         temp = self._data["activity"]
-        temp = temp.replace("-", " ").replace("_", " ")
+        temp = temp.replace("-", " ")
         return temp
 
-
-class EPICDatasetClip(Dataset):
-    def __init__(self,
-                 split="train",
-                 activity_set=None,
-                 vocab_encoder=None,
-                 tokenized_vocab=None,
-                 mean=[0., 0., 0.],
-                 std=[1., 1., 1.]
-                 ) -> None:
+class EpicKitchen55(Dataset):
+    def __init__(self, 
+                split = "train",
+                activity_set = None,
+                vocab_encoder = None,
+                tokenized_vocab = None,
+                mean = [0., 0., 0.],
+                std = [1., 1., 1.]
+                ) -> None:
         super(Dataset, self).__init__()
         self.split = split
         self.activity_set = activity_set
         self.vocab_encoder = vocab_encoder
         self.tokenized_vocab = tokenized_vocab
-
+        
         self.dataset_name = f"EPIC-Kitchen Dataset {self.split}"
         self.image_path = "/data/EPIC-KITCHENS/images/"
         annotation_file = f"/data/EPIC-KITCHENS/annotations/action_{self.split}_labels_clip_128_64.csv"
         self.df = loadDatasetDescription(annotation_file)
         self.vocab = json.load(open("src/dataset/vocab.json"))
         self.updateDF()
-
+        
         self.mean = mean
         self.std = std
-
+                
         with Pool(processes=8) as pool:
-            self.video_list = pool.starmap(createVideoRecord, [(
-                row, self.image_path) for _, row in self.df.iterrows()])
-
+            self.video_list = pool.starmap(createVideoRecord, [(row, self.image_path) for _, row in self.df.iterrows()])
+        
+        
         print(f"Dataset contains a total of {len(self.video_list)} clips")
-
+    
     def __len__(self):
         return len(self.df)
 
@@ -94,32 +92,34 @@ class EPICDatasetClip(Dataset):
             idx: int
         """
         record: VideoRecord = self.video_list[idx]
-
-        filename = os.path.join(record.path,
+        
+        filename = os.path.join(record.path, 
                                 f"frame_{str(record.start_frame).rjust(10, '0')}-{str(record.end_frame).rjust(10, '0')}.safetensor")
-
+        
         tensors = load_file(filename)
-
+        
         images = tensors["tensor"]
         label = torch.tensor(record.label)
-
-        tokens = torch.tensor(self.vocab[record.activity], dtype=torch.long)
+        
+        tokens = torch.zeros(4, dtype=torch.long)
+        temp = torch.tensor(self.vocab[record.activity], dtype=torch.long)
+        tokens[:len(temp)] = temp
 
         images = images.type(torch.float32)
         images /= 255
-
+        
         images = images.permute(1, 0, 2, 3)
-
+        
         images = F.normalize(images, mean=self.mean, std=self.std)
-
+        
         images = images.permute(1, 0, 2, 3)
-
+        
         return {
             "tensor": images[:, 32:96],
             "tokens": tokens,
             "labels": label
         }
-
+        
     def updateDF(self):
         self.df["length"] = self.df["stop_frame"] - self.df["start_frame"] + 1
 
@@ -134,8 +134,7 @@ class EPICDatasetClip(Dataset):
                 temp = torch.tensor(self.vocab[item], dtype=torch.long)
                 self.tokenized_vocab[i, :len(temp)] = temp
 
-        self.df["label"] = self.df["activity"].apply(lambda x: self.activity_set.index(
-            x.replace("-", " ").replace("_", " ")) if x.replace("-", " ").replace("_", " ") in self.activity_set else -1)
+        self.df["label"] = self.df["activity"].apply(lambda x: self.activity_set.index(x.replace("-", " ")) if x.replace("-", " ") in self.activity_set else -1)
 
         sum_error_labels = sum(self.df["label"] < 0)
         print(f"Warning: Number of error labels: {sum_error_labels}")
@@ -143,29 +142,14 @@ class EPICDatasetClip(Dataset):
         # filter error labels to avoid missmatch between train/test dataset and longer clips
         self.df = self.df[(self.df["label"] >= 0) & (127 < self.df["length"])]
 
-    def generate_batch(self, data_batch):
-        images = torch.stack([element["tensor"] for element in data_batch])
-        labels = torch.stack([element["labels"] for element in data_batch])
-        tokens = pad_sequence([element["tokens"] for element in data_batch], batch_first=True, padding_value=self.vocab["<pad>"][0])
-
-        return {
-                "tensor": images,
-                "tokens": tokens,
-                "labels": labels
-            }
-
 def createVideoRecord(row, image_path):
     return VideoRecord(row, image_path)
-
 
 def loadDatasetDescription(annotation_file):
     with open(annotation_file) as f:
         df = pd.read_csv(f, delimiter=',', encoding='utf-8', header=0)
-        df = df[["participant_id", "video_id", "start_frame", "stop_frame",
-                 "verb", "verb_class", "noun", "noun_class", "activity"]]
+        df = df[["participant_id", "video_id", "start_frame", "stop_frame", "verb", "verb_class", "noun", "noun_class", "activity"]]
         return df
-
-
 
 def get_dataloader(dataset, batch_size, dev):
     """ Get a dataloader from a dataset.
@@ -173,25 +157,21 @@ def get_dataloader(dataset, batch_size, dev):
     shuffle = True if dataset.split == "train" else False
     drop_last = True if dataset.split == "train" else False
     dataloader = DataLoader(
-        dataset if not dev else Subset(dataset, random.sample(
-            range(len(dataset)), batch_size * 50)),
+        dataset if not dev else Subset(dataset, random.sample(range(len(dataset)), batch_size * 50)),
         batch_size=batch_size,
         shuffle=shuffle,
         drop_last=drop_last,
         num_workers=0,
         pin_memory=True,  # set True when loading data on CPU and training on GPU
-        collate_fn=dataset.generate_batch
     )
     return dataloader
 
-
+ 
 def prepare_data(batch_size=32, debug=False):
     """ Prepare data.
     """
-    train_dataset = EPICDatasetClip(
-        "train", mean=[0.3794, 0.3206, 0.2973], std=[0.2150, 0.2096, 0.2016])
-    test_dataset = EPICDatasetClip("test", mean=[0.3958, 0.3278, 0.3068], std=[
-                                   0.2193, 0.2103, 0.2025], activity_set=train_dataset.activity_set, vocab_encoder=train_dataset.vocab_encoder, tokenized_vocab=train_dataset.tokenized_vocab)
+    train_dataset = EpicKitchen55("train", mean=[0.3794, 0.3206, 0.2973], std=[0.2150, 0.2096, 0.2016])
+    test_dataset = EpicKitchen55("test", mean=[0.3958, 0.3278, 0.3068], std=[0.2193, 0.2103, 0.2025], activity_set=train_dataset.activity_set, vocab_encoder=train_dataset.vocab_encoder, tokenized_vocab=train_dataset.tokenized_vocab)
     train_dl = get_dataloader(train_dataset, batch_size, debug)
     test_dl = get_dataloader(train_dataset, batch_size, True)
     eval_dl = get_dataloader(test_dataset, batch_size, debug)
@@ -201,4 +181,22 @@ def prepare_data(batch_size=32, debug=False):
         "test_dl": test_dl,
         "eval_dl": eval_dl,
         "tokenized_vocab": train_dataset.tokenized_vocab
-    }
+    }       
+
+if __name__ == "__main__":
+    dataset = EpicKitchen55("train")
+    print(dataset.df.iloc[:5])
+    
+    it = dataset[5]
+    print(it["tensor"].shape)
+    print(it["tensor"][0][0])
+    print(it["tokens"].shape)
+    print(it["tokens"].dtype)
+    print(len(dataset))
+    print(dataset.tokenized_vocab.shape)
+    
+    data = prepare_data(32, 600)
+    train_dl = data["train_dl"]
+    test_dl = data["test_dl"]
+    
+    batch = next(iter(train_dl))
