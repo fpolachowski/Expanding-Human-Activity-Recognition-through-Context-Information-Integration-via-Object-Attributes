e3815b83fd22e661d75c2e23dfeed59ecd49ff84
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2024-11-03 15:08:40 +0000

    switched to old version of CHAR

--- src/model/CHAR.py
+++ src/model/CHAR.py
@@ -13,6 +13,7 @@ class CHAR(nn.Module):
             sentence_length=4,
             object_count=16,
             vocab_length=1596,
+            vocab_size=49383,
             transformer_width=512,
             transformer_layers=6,
             transfromer_nheads=8,
@@ -29,22 +30,18 @@ class CHAR(nn.Module):
         self.object_encoding = nn.Parameter(torch.empty(object_count, transformer_width))
         self.object_encoder = nn.TransformerEncoder(encoder_layers, transformer_layers)
         self.object_ln = nn.LayerNorm(transformer_width)
-        self.object_pool = AttentionPooling(transformer_width, transfromer_nheads)
         
         # Query Text Section
-        self.query_embedding = nn.Embedding(vocab_length, transformer_width)
+        self.query_embedding = nn.Embedding(vocab_size, transformer_width)
         self.query_encoding = nn.Parameter(torch.empty(sentence_length, transformer_width))
         self.query_encoder = nn.TransformerEncoder(encoder_layers, transformer_layers)
         self.query_ln = nn.LayerNorm(transformer_width)
-        self.query_pool = AttentionPooling(transformer_width, transfromer_nheads)
 
         # Vision Section
-        # self.frame_extractor = FrameFeatureExtractor(transformer_width)
         self.frame_extractor = ResNetFrameFeatureExtractur("resnet50", transformer_width)
         self.frame_encoding = nn.Parameter(torch.empty(video_length, transformer_width))
         self.frame_encoder = nn.TransformerEncoder(encoder_layers, transformer_layers)
         self.frame_ln = nn.LayerNorm(transformer_width)
-        self.frame_pool = AttentionPooling(transformer_width, transfromer_nheads)
         
         # cross-modal section
         self.cross_modal_vid_txt_encoder = QueryVideoCrossModalEncoder(transformer_width, 2, dropout)
@@ -76,12 +73,6 @@ class CHAR(nn.Module):
                 nn.init.normal_(layer.self_attn.out_proj.weight, std=proj_std)
                 nn.init.normal_(layer.linear1.weight, std=fc_std)
                 nn.init.normal_(layer.linear2.weight, std=proj_std)
-                
-        for pooling in [self.query_pool, self.frame_pool, self.object_pool]:
-            if pooling.attn is not None:
-                std = pooling.attn.out_proj.in_features ** -0.5
-                nn.init.normal_(pooling.attn.in_proj_weight, std=std)
-                nn.init.normal_(pooling.attn.out_proj.weight, std=std)
             
     def pooling(self, x, dim):
         return torch.max(x, dim=dim)[0]
@@ -93,7 +84,7 @@ class CHAR(nn.Module):
         object_features = torch.reshape(object_features, (B*N, O, D))
         object_features = self.object_encoder(object_features)
         object_features = self.object_ln(object_features)
-        object_features = self.object_pool(object_features)
+        object_features = self.pooling(object_features, dim=1)
         object_features = torch.reshape(object_features, (B, N, D))
         
         return object_features
@@ -112,9 +103,7 @@ class CHAR(nn.Module):
         words_feature = self.query_ln(words_feature)
         return words_feature
     
-    def cross_modality(self, frame_features, words_features, object_features):
-        frame_features, _ = self.cross_modal_vid_obj_encoder(frame_features, object_features)
-        
+    def cross_modality(self, frame_features, words_features):
         video_features = torch.empty((frame_features.shape[0], words_features.shape[0], frame_features.shape[2]), device=frame_features.device)
         sentence_features = torch.empty((frame_features.shape[0], words_features.shape[0], frame_features.shape[2]), device=words_features.device)
         
@@ -123,8 +112,8 @@ class CHAR(nn.Module):
             
             _words_feature, _single_vid_features = self.cross_modal_vid_txt_encoder(words_features, _single_vid_frame_features)
             
-            video_features[i] = self.frame_pool(_single_vid_features)
-            sentence_features[i] = self.query_pool(_words_feature)
+            video_features[i] = self.pooling(_single_vid_features, dim=1)
+            sentence_features[i] = self.pooling(_words_feature, dim=1)
             
         return video_features, sentence_features
 
@@ -137,25 +126,44 @@ class CHAR(nn.Module):
         frame_features = self.encode_video(images)
         words_features = self.encode_text(texts)
         object_features = self.encode_object(pred_det)
-
-        # cross modality video <- object + video <-> text
-        video_features, sentence_features = self.cross_modality(frame_features, words_features, object_features)
+        
+        # cross modality video <- object
+        frame_features, object_features = self.cross_modal_vid_obj_encoder(frame_features, object_features)
+        
+        _frame_features = frame_features / frame_features.norm(dim=2, keepdim=True)
+        object_features = object_features / object_features.norm(dim=2, keepdim=True)
+        
+        video_object_similarity = F.cosine_similarity(_frame_features, object_features, dim=-1)
+        video_object_sim = torch.mean(video_object_similarity)
+        
+        # cross modality video <-> text
+        video_features, sentence_features = self.cross_modality(frame_features, words_features)
         
         # Normalize IMPORTANT!!!
         video_features = video_features / video_features.norm(dim=2, keepdim=True)
         sentence_features = sentence_features / sentence_features.norm(dim=2, keepdim=True)
 
         # cosine similarity as logits
-        video_text_similarity = F.cosine_similarity(video_features, sentence_features, dim=-1)
+        logit_scale = self.logit_scale.exp()
+        video_text_similarity = logit_scale * F.cosine_similarity(video_features, sentence_features, dim=-1)
         video_text_loss = F.cross_entropy(video_text_similarity, labels)
+        
+        # Video text similarity for tracking
+        video_text_similarity = F.cosine_similarity(video_features, sentence_features, dim=-1)
         
         return video_text_loss, video_text_similarity
     
-    def forward_eval(self, images, words_features, object_features):
+    def forward_eval(self, images, words_features, object_pred):
         frame_features = self.encode_video(images)
-        object_features = self.encode_object(object_features)
+        object_features = self.encode_object(object_pred)
+        frame_features, object_features = self.cross_modal_vid_obj_encoder(frame_features, object_features)
         
-        video_features, sentence_features = self.cross_modality(frame_features, words_features, object_features)        
+        video_features, sentence_features = self.cross_modality(frame_features, words_features)
+        
+        # Normalize IMPORTANT!!!
+        video_features = video_features / video_features.norm(dim=2, keepdim=True)
+        sentence_features = sentence_features / sentence_features.norm(dim=2, keepdim=True)
+        
         return video_features, sentence_features
 
     def load_checkpoint(self, exp_folder_path, suffix):
